c4beed8498e3e5aca39591af823d9291f91e1bd5
enzopavanelli <pcenzopavanelli@hotmail.com>
2023-10-19 19:11:49 -0300

    chore: challenge complete

---
+++ src/utils/build-route-path.js
@@ -0,0 +1,12 @@
+export function buildRoutePath(path) {
+  const routeParamentersRegex = /:([a-zA-Z]+)/g;
+
+  const pathWithParams = path.replaceAll(
+    routeParamentersRegex,
+    "(?<$1>[a-z0-9-_]+)"
+  );
+
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
+
+  return pathRegex;
+}
